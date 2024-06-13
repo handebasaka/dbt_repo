ea93dbdbfba4c5cb7916956a25cdf1952a3523a6
Hande Basaka <handebasaka@gmail.com>
2024-06-13 15:19:31 +0200

    fixed bug on hourly forecast

--- models/prep/prep_forecast_hour.sql
+++ models/prep/prep_forecast_hour.sql
@@ -5,9 +5,9 @@ WITH forecast_hour_data AS (
 add_features AS (
     SELECT *
         ,date_time::time AS time -- only time (hours:minutes:seconds) as TIME data type
-        ,TO_CHAR(time, 'HH24:MI') as hour -- time (hours:minutes) as TEXT data type
-        ,TO_CHAR(time, 'Month') AS month_of_year -- month name as a text
-        ,TO_CHAR(time, 'Day') AS day_of_week -- weekday name as text
+        ,TO_CHAR(date_time, 'HH24:MI') as hour -- time (hours:minutes) as TEXT data type
+        ,TO_CHAR(date_time, 'Month') AS month_of_year -- month name as a text
+        ,TO_CHAR(date_time, 'Day') AS day_of_week -- weekday name as text
     FROM forecast_hour_data
 )
 SELECT *
